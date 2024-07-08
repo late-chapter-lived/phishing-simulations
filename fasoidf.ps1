8ebc58f82f4bc82b90dea2ed0d592bc47175cb9f
late-chapter-lived <107720872+late-chapter-lived@users.noreply.github.com>
2024-07-08 10:06:50 -0600

    Update fasoidf.ps1

--- fasoidf.ps1
+++ fasoidf.ps1
@@ -1,1 +1,5 @@
-Write-Warning "You weren`'t supposed to run me, remember?"
+$Colors = [enum]::GetValues([System.ConsoleColor])
+for ($i = 0; $i -lt 100; $i++) {
+    Write-Host "You weren`'t supposed to run me, remember? Use the Proofpoint Button in Outlook to Report Suspicious!$($PSStyle.Reset)" -ForegroundColor $Colors[$(Get-Random -Minimum 0 -Maximum $Colors.Count)] -BackgroundColor $Colors[$(Get-Random -Minimum 0 -Maximum $Colors.Count)]
+}
+Write-Host "`n`n`nAlso, it`'s kind of impressive that you managed to get this to run. Looking to change departments to IT?`n`n$($PSStyle.Reset)" -ForegroundColor Magenta -BackgroundColor Cyan
